12145d41b5928f0622186736324f69633a3be79f
Chris Flint <flintedc@gmail.com>
2016-07-19 14:36:15 +0100

    display working

--- ajax_startpoint/public/index.js
+++ ajax_startpoint/public/index.js
@@ -1,6 +1,6 @@
-window.onload = function(){
-  console.log("window loaded");
 
+
+window.onload = function(){
   var url = 'https://restcountries.eu/rest/v1';
   var request = new XMLHttpRequest();
   request.open( "GET", url );
@@ -8,12 +8,48 @@ window.onload = function(){
 
     if (request.status === 200){
       var jsonString = request.responseText;
-      var countries = JSON.parse(jsonString);
-      var country = countries[5];
-      console.log(country);
+      countries = JSON.parse(jsonString);
       }
+    main();
+  }
+  request.send( null );
+};
+
+
+function main(){
+  countries.forEach(function(country){
+    var option = document.createElement('option');
+    option.innerHTML = country.name;
+    var select = document.getElementById('selector');
+    select.appendChild(option);
+  })
+
+  var button = document.getElementById('select-button');
+  var form = document.getElementById('dropdown');
+  button.onclick = function(event){
+    var current = document.getElementById('selector').value;
+    displayCountry(current);
   }
 
-  request.send( null );
-  // console.log(request);
+  form.onsubmit = function(event){
+    event.preventDefault();
+  }
+
+  function displayCountry(current){
+    var found = countries.filter(function(country) { return country.name === current; });
+    createEntry(found[0]);
+  }
+
+  function createEntry(found){
+    var li = document.createElement('li');
+    var hr = document.createElement('hr');
+    li.innerHTML =  "<b>Name:</b> " + found.name + " <b>Capital:</b> " + found.capital + " <b>Pop:</b> " + found.population
+    var ul = document.getElementById('country-list');
+    // ul.firstChild.innerHTML = "Name: " + found.name + " Capital: " + found.capital + " Pop: " + found.population;
+    ul.appendChild(li);
+    li.appendChild(hr);
+  }
+
+
+
 }
